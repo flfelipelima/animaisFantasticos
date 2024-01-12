5dbf455d7d6da34edf901454cae8500935f981a3
flfelipelima <mota.felipelima@gmail.com>
2024-01-12 16:48:48 -0300

    added smooth scrolling

--- script.js
+++ script.js
@@ -40,3 +40,20 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
